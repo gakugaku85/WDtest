5c12aa1b2e4bc210df8c36fd41fa88fdaab3ff30
sekiguchi gaku <s238319q@st.go.tuat.ac.jp>
2023-11-08 01:45:46 +0000

    濃度プロファイルを表示するようにした

--- create_img.py
+++ create_img.py
@@ -4,10 +4,11 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import SimpleITK as sitk
 
-def calculate_snr(image):
+def calculate_snr(image, original_image):
     signal_mean = np.mean(image)
-    noise = image - signal_mean
+    noise = image - original_image
     noise_std = np.std(noise)
     snr = 20 * np.log10(signal_mean / noise_std)
     return snr
@@ -19,6 +20,37 @@ def save_image_hist(image, filename):
     plt.ylabel("Count")
     plt.savefig("{}_hist.png".format(filename))
 
+def concentration_profile(image, filename):
+    # # x direction
+    # x_vals = np.linspace(0, image.shape[0] - 1, image.shape[0])
+    # y_vals = np.linspace(32, 32, image.shape[1])
+    # pixel_values = [image[int(y), int(x)] for x, y in zip(x_vals, y_vals)]
+
+    # plt.clf()
+    # plt.plot(pixel_values, label="Pixel values along the line", color='blue')
+    # plt.title("Pixel Values along the Center Line")
+    # plt.xlabel("Position along the line")
+    # plt.ylabel("Pixel Value")
+    # plt.savefig("{}_gram_x.png".format(filename))
+
+    # y direction
+    x_vals = np.linspace(0, 0, 13)
+    y_vals = np.linspace(25, 38, 13)
+    pixel_values = []
+    for x, y in zip(x_vals, y_vals):
+        av_pixel = 0
+        for i in range(64):
+            av_pixel += image[int(y), int(i)]
+        av_pixel /= 10
+        pixel_values.append(av_pixel)
+
+    plt.clf()
+    plt.plot(pixel_values, label="Pixel values along the line", color='blue')
+    plt.title("Pixel Values along the Center Line")
+    plt.xlabel("Position along the line")
+    plt.ylabel("Pixel Value")
+    plt.savefig("{}_gram_y.png".format(filename))
+
 os.makedirs("images", exist_ok=True)
 
 image = np.zeros((64, 64)) + 64
@@ -27,27 +59,30 @@ image[center_line - 2 : center_line + 1, :] = 192
 
 Image.fromarray(image.astype(np.uint8)).save("images/original_pil.png")
 save_image_hist(image, "images/original_image")
-
+concentration_profile(image, "images/original_image")
+sitk.WriteImage(sitk.GetImageFromArray(image), "images/original_image.mhd")
 
 kennel_size = 7
 mean = 0
 blur_sigma = 1
-noise_sigma = 128
+noise_sigma = 32
 
 blurred_image = cv2.GaussianBlur(image, (kennel_size, kennel_size), blur_sigma)
 
 noise = np.random.normal(mean, noise_sigma, blurred_image.shape).astype(np.float64)
 noisy_image = cv2.add(blurred_image, noise)
 #0~255に正規化
-noisy_image = (noisy_image - np.min(noisy_image)) / (np.max(noisy_image) - np.min(noisy_image)) * 255
+# noisy_image = (noisy_image - np.min(noisy_image)) / (np.max(noisy_image) - np.min(noisy_image)) * 255
 # noisy_image = np.clip(noisy_image, 0, 255).astype(np.float64)
 
-Image.fromarray(noisy_image.astype(np.uint8)).save("images/noisy_pil" + "_bsigma=" + str(blur_sigma) + "_nsigma=" + str(noise_sigma) + ".png")
-save_image_hist(noisy_image, "images/noisy_" + "_bsigma=" + str(blur_sigma) + "_nsigma=" + str(noise_sigma))
+Image.fromarray(noisy_image.astype(np.uint8)).save("images/noisy" + "_bsigma=" + str(blur_sigma) + "_nsigma=" + str(noise_sigma) + "_pil.png")
+save_image_hist(noisy_image, "images/noisy" + "_bsigma=" + str(blur_sigma) + "_nsigma=" + str(noise_sigma))
+concentration_profile(noisy_image, "images/noisy" + "_bsigma=" + str(blur_sigma) + "_nsigma=" + str(noise_sigma))
+sitk.WriteImage(sitk.GetImageFromArray(noisy_image), "images/noisy_" + "_bsigma=" + str(blur_sigma) + "_nsigma=" + str(noise_sigma) + ".mhd")
 
-original_snr = calculate_snr(image)
-noisy_snr = calculate_snr(noisy_image)
-print(f"元の画像のSN比: {original_snr} dB")
+# original_snr = calculate_snr(image, image)
+# print(f"元の画像のSN比: {original_snr} dB")
+noisy_snr = calculate_snr(noisy_image, image)
 print(f"ノイズを加えた後のSN比: {noisy_snr} dB")
 
 
