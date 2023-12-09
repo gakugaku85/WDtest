599a8db038bae1b17080c80117aeffc7d31b5b5d
sekiguchi gaku <s238319q@st.go.tuat.ac.jp>
2023-12-09 14:47:44 +0000

    count_ccで残った連結成分の画像を出力するようにした 不要なファイルの削除

--- count_cc.py
+++ count_cc.py
@@ -3,6 +3,8 @@ from skimage import measure
 import SimpleITK as sitk
 import os
 import matplotlib.pyplot as plt
+import argparse
+import cv2
 from natsort import natsorted
 
 
@@ -10,14 +12,25 @@ def binary_threshold(image, threshold):
     binary_image = (image > threshold).astype(np.uint8)
     return binary_image
 
-def connected_components(image, original_image):
+def create_connected_components_image(image, original_image, fname, dir_path):
+    os.makedirs(dir_path + "/images/", exist_ok=True)
+    cv2.imwrite(dir_path + "/images/" + fname.split(".")[0] + "_original" + ".png", original_image * 255)
+    cv2.imwrite(dir_path + "/images/" + fname.split(".")[0] + "_binary" + ".png", image * 255)
+
     labeled_image = measure.label(image, connectivity=2)
-    labeled_image = labeled_image * original_image
-    labels = np.unique(labeled_image)[1:]
+    mix_labeled_image = labeled_image * original_image
+    cv2.imwrite(dir_path + "/images/" + fname.split(".")[0] + "_labeled" + ".png", labeled_image * 255)
+
+    # 残ったラベルのみを抽出
+    labels = np.unique(mix_labeled_image)[1:]
     num_components = len(labels)
+    label_image = np.zeros_like(original_image)
+    for label in labels:
+        label_image[labeled_image == label] = 1
+    cv2.imwrite(dir_path + "/images/" + fname.split(".")[0] + "_label" + ".png", label_image * 255)
     return num_components
 
-def main():
+def main(path, sigma):
     threshold_value = 128
 
     original_val1_path = "images/original_val1/"
@@ -41,7 +54,8 @@ def main():
                 original_val2_images.append(original_image)
 
 
-    result_path = "../SR3/SR3_wdTest/experiments/wdtest_16_64_231206_005037"
+    result_path = "../SR3/SR3_wdTest/experiments/"
+    result_path = result_path + path
     result_path = result_path + "/results/"
     assert os.path.isdir(result_path), '{:s} is not a valid directory'.format(result_path)
 
@@ -59,7 +73,7 @@ def main():
                     image = sitk.GetArrayFromImage(sitk.ReadImage(mhd_file_path))
                     image = image[:, 64:128]
                     binary_image = binary_threshold(image, threshold_value)
-                    num_components.append(connected_components(binary_image, binary_threshold(original_val1_images[i], threshold_value)))
+                    num_components.append(create_connected_components_image(binary_image, binary_threshold(original_val1_images[i], threshold_value), fname, dir_path))
                     i += 1
             avg_val1_components.append(np.mean(num_components))
             print(f"val1平均連結成分数: {np.mean(num_components)}")
@@ -74,12 +88,13 @@ def main():
                     image = sitk.GetArrayFromImage(sitk.ReadImage(mhd_file_path))
                     image = image[:, 64:128]
                     binary_image = binary_threshold(image, threshold_value)
-                    num_components.append(connected_components(binary_image, binary_threshold(original_val2_images[i], threshold_value)))
+                    num_components.append(create_connected_components_image(binary_image, binary_threshold(original_val2_images[i], threshold_value), fname, dir_path))
                     i += 1
             avg_val2_components.append(np.mean(num_components))
             print(f"val2平均連結成分数: {np.mean(num_components)}")
     plt.plot(avg_val1_components, label="val1")
     plt.plot(avg_val2_components, label="val2")
+    plt.title("sigma={} TPCC".format(sigma))
     plt.legend()
     plt.xlabel("validation num")
     plt.ylabel("number of mean connected components")
@@ -87,4 +102,8 @@ def main():
     plt.clf()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", type=str, help="path to result folder")
+    parser.add_argument("-s", "--sigma", type=int, default=64 ,help="sigma value")
+    args = parser.parse_args()
+    main(args.path, args.sigma)
