6b54a6833820335cdd493b9632d2c6a4ec946605
sekiguchi gaku <s238319q@st.go.tuat.ac.jp>
2024-02-02 05:31:26 +0000

    thresholdとか諸々変更

--- pd_create_all.py
+++ pd_create_all.py
@@ -53,41 +53,73 @@ def save_persistent_diagram(persistence, output_dir, output_file_name):
     plt.ylim(0, 260)
     output_file_path = output_dir + output_file_name + ".png"
     plt.savefig(output_file_path)
+    plt.close()
     return output_file_path
 
-def homologize_persistence(image_data, persistence, output_dir, output_file_name):
+def homologize_persistence(image_data, persistence, output_dir, output_file_name, each_save=False, image_name="HR"):
     os.makedirs(output_dir+"/point/", exist_ok=True)
     for i, (betch, (birth, death)) in enumerate(persistence):
         if betch == 0:#連結成分のみ
-            if death - birth < 30:
+            if death - birth < 200:
                 continue
-            #deathの半分で二値化
-            print("birth:", birth, "death:", death)
-            save_persistent_diagram([(betch, (birth, death))], output_dir+"/point/", output_file_name+str(i))
-            threshold = death // 2
+
+            save_persistent_diagram([(betch, (birth, death))], output_dir+"/point/", output_file_name+str(i)+"_pd_"+image_name)
+            threshold = (death + birth) // 2
             thre_bin_image = binary_threshold(image_data, threshold)
-            cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_point.png", thre_bin_image*255)
 
             #deathの半分で二値化したものを元に、ラベリング
             label = measure.label(thre_bin_image, connectivity=2)
-            print("label:", label.max(), label.min())
 
             #ラベリングしたものを元に、ラベルごとに色を付ける
-            image_label_overlay = label2rgb(label, image=thre_bin_image)
-            cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_point_color.png", image_label_overlay*255)
+            image_label_overlay = label2rgb(label, bg_label=0)
 
             #deathの直前の閾値で二値化
-            threshold = death - 1
+            threshold = death - birth - 1
+            print(image_data.max())
             death_before_image = binary_threshold(image_data, threshold)
-            cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_death_before.png", death_before_image*255)
+            death_image = binary_threshold(image_data, death)
 
-            #deathの直前の閾値で二値化した画像の255の部分をhalf_thre_bin_imageから取り出す
+            sub_image = death_before_image - death_image
+
+            #deathの直前の閾値で二値化した画像の255の部分を、ラベリングした画像のラベルに置き換える
             mix_labeled_image = label * death_before_image
             labels = np.unique(mix_labeled_image)[1:]
             label_image = np.zeros_like(thre_bin_image)
             for lab in labels:
                 label_image[label == lab] = lab
-            cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_death_before_res.png", label_image*255)
+            print(image_name, "label:", len(labels), "death:", death, "birth:", birth, output_file_name+str(i)+"_pd_"+image_name)
+            colored_labels = label2rgb(label_image, bg_label=0)
+
+            #最も面積の大きいラベルのみを抽出
+            # labels = np.unique(label_image)[1:]
+            # most_large_label = labels.count_max()
+            # print("most_large_label:", most_large_label)
+
+            # label_image_max = np.zeros_like(thre_bin_image)
+            # for lab in labels:
+            #     label_image_max[label == lab] = lab
+            # colored_labels_max = label2rgb(label_image_max, bg_label=0)
+
+            #画像にタイトルを付ける
+            if each_save:
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_point_color.png", image_label_overlay*255)
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_point.png", thre_bin_image*255)
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_death.png", death_image*255)
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_death_before.png", death_before_image*255)
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_extract_color.png", colored_labels*255)
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_extract.png", label_image*255)
+                cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_death_sub.png", sub_image*255)
+
+            #白黒画像を拡張
+            thre_bin_image = cv2.cvtColor(thre_bin_image, cv2.COLOR_GRAY2BGR)
+            death_before_image = cv2.cvtColor(death_before_image, cv2.COLOR_GRAY2BGR)
+            image_label_overlay = image_label_overlay.astype(np.uint8)
+            colored_labels = colored_labels.astype(np.uint8)
+            # colored_labels_max = colored_labels_max.astype(np.uint8)
+
+            #画像を横に並べる
+            concat_image = cv2.hconcat([thre_bin_image*255, death_before_image*255, image_label_overlay*255, colored_labels*255])
+            cv2.imwrite(output_dir+"/point/"+output_file_name+str(i)+"_concat_"+image_name+".png", concat_image)
 
 
 def create_persistent_diagram(ori_image_data, fname, dir_path):
@@ -95,6 +127,7 @@ def create_persistent_diagram(ori_image_data, fname, dir_path):
     image_list = ["LR", "SR", "HR"]
     output_dir = dir_path + "/pd"
     os.makedirs(output_dir, exist_ok=True)
+    ori_image_data = ori_image_data.astype(np.uint8)
 
     output_file_name = fname[:-4] + "_pd"
     image_paths = []
@@ -110,9 +143,7 @@ def create_persistent_diagram(ori_image_data, fname, dir_path):
             if death[1] == float("inf"):
                 persistence[idx] = (birth, (death[0], 255))
 
-        if image_name == "HR":
-            homologize_persistence(image_data, persistence, output_dir, output_file_name)
-            input("Press Enter to continue...")
+        homologize_persistence(image_data, persistence, output_dir, output_file_name, image_name=image_name)
 
         output_file_path = save_persistent_diagram(persistence, output_dir, output_file_name + "_" + image_name)
         image_paths.append(output_file_path)
@@ -130,7 +161,7 @@ def main(path):
     assert os.path.isdir(result_path), '{:s} is not a valid directory'.format(result_path)
 
     for dir_path, _, fnames in natsorted(os.walk(result_path)):
-        if dir_path.split("/")[-1] == "val1" or dir_path.split("/")[-1] == "val2":
+        if (dir_path.split("/")[-1] == "val1" or dir_path.split("/")[-1] == "val2") and dir_path.split("/")[-2] == "10000":
             for fname in natsorted(fnames):
                 if fname.endswith(".mhd"):
                     mhd_file_path = os.path.join(dir_path, fname)
